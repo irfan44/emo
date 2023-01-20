2d45a7ca6bc95bd30283999af2bf727371f7d9a9
irfan44 <irfannmuhajir@gmail.com>
2023-01-20 10:35:30 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+
+	async function hello() {
+		// @ts-ignore
+		console.log(await window.electronAPI.getAccessToken());
+	}
+
+	hello();
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-bold">Info alert!</span> Change a few things up and try submitting again.
+	</Alert>
+</div>
